9ba5a3f9e362fd93bfc4c91281ad82074bb8ebe1
Javier Gomez <jgmontero>
2022-08-14 10:26:31 -0400

    add(major): news service added

---
+++ src/app/services/news-services.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsServicesService {
+  httpOptions = {
+    headers: {}
+  };
+
+  constructor(private client: HttpClient) {
+  }
+
+  getNews(languaje: string, page: number): Observable<any> {
+    const url = 'https://hn.algolia.com/api/v1/search_by_date?query=' + languaje + '&page=' + page;
+    return this.client.get<any[]>(url, this.httpOptions);
+  }
+}
