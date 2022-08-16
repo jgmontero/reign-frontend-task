c1593a2e0c1523f0d7e18cd4e5d921c24930ea98
Javier Gomez <jgmontero>
2022-08-16 02:36:59 -0400

    test(minor):  ng test ran               unnused component removed

--- src/app/services/news-services.service.spec.ts
+++ src/app/services/news-services.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NewsServicesService } from './news-services.service';
+import {} from "@angular/common/http";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('NewsServicesService', () => {
   let service: NewsServicesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers:[NewsServicesService],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
+    });
     service = TestBed.inject(NewsServicesService);
   });
 
